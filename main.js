03799689a5688f9932801414ce5a840a45dda723
Heather <lilpinkrose02@gmail.com>
2022-10-28 14:00:38 -0500

    created the animation for the submit button. Have not yet set it up to happen on incorrent password.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-const signUpBtn = document.querySelector('.signup-btn')
+const signInBtn = document.querySelector('.signin-btn')
+
+signInBtn.addEventListener('click', () => {
+    const btnClassList = signInBtn.classList
+
+    if(btnClassList.contains('center')) {
+        btnClassList.remove('center') 
+        btnClassList.add('left')
+    }else if(btnClassList.contains('left')){
+        btnClassList.remove('left')
+        btnClassList.add('toCenter')
+    }else if(btnClassList.contains('toCenter')){
+        btnClassList.remove('toCenter')
+        btnClassList.add('right')
+    }else if(btnClassList.contains('right')){
+        btnClassList.remove('right')
+        btnClassList.add('center')
+    }
+})
+
+
