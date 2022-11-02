f21f5c3107c85b17d3f5cf70b0cfba9abbf11fae
Heather <lilpinkrose02@gmail.com>
2022-11-01 20:53:26 -0500

    connected sign in to database

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // const http = require('http')
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+
+const dbUrl = 'mongodb+srv://Hdavis73:Heather1@logininfo.zv7mcrt.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => console.log('connected to the database'))
 
 app.set('view engine', 'ejs')
 
